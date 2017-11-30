6b285d439782f42950f4f36e38d442569709b7b6
Vinicius Santos <viniciussantos1924@gmail.com>
2017-11-30 03:03:41 -0200

    - comment removed

--- index.js
+++ index.js
@@ -28,21 +28,6 @@ app.use(bodyParser.urlencoded({extended: true})); // para analisar application/x
 app.use(bodyParser.json()); // para analisar application/json
 app.use(cors());
 
-/* 
- * Ao executar esse arquivo no node: 
- * 
- * Primeiramente ele vai requerer o módulo "server"(que é onde está a configuração
- * e inicialização do meu servidor) e vai armazenar na constante "server"(definida aqui 
- * nesse arquivo) o que foi exportado dentro do arquivo "server.js"
- * 
- * Depois ele vai requerer o módulo "database"(que é onde está minha conexão com o mysql
- * e os modelos do banco de dados)
- * 
- * Depois ele vai requerer o módulo "api_routes"(que define que todas as rotas dentro do
- * caminho "/api" devem ser acessadas através de um token) e vai passar como parâmetro o que foi 
- * exportado dentro do arquivo "server.js"
- */
-
 const database = require("./config/database");
 require("./api/routes/api_routes")(app, router, database);
 require("./api/routes/unprotected/login")(app, database);
