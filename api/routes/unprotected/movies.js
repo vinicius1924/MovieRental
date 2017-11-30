dc216d1432ee51e3d773737956af8cd7610801d6
Vinicius Santos <viniciussantos1924@gmail.com>
2017-11-30 12:42:14 -0200

    - add files with routes protection removed

---
+++ api/routes/unprotected/movies.js
@@ -0,0 +1,27 @@
+module.exports = (app, database) =>
+{
+   app.route("/movies")
+   .get((req, res) =>
+   {
+      database.sequelize.query("SELECT movie.id, movie.title, movie.director, " +
+      "(movie.number_of_copies - movie.located_copies) " +
+      "AS available_copies " +
+      "FROM movie",
+      { 
+         type: database.sequelize.QueryTypes.SELECT
+      })
+      .then((movies) => 
+      {
+         if(movies.length != 0)
+         {
+            res.status(200).json({movies});
+         }
+         else
+         {
+            let errors = [];
+            errors.push("no movies found");
+            res.status(404).json({errors});
+         }
+      });
+   });
+};
